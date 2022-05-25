e5057c9f048bf21047d3351a4e6465c2f6ed9084
Juan Salvucci <juani.salvucci@gmail.com>
2022-05-25 20:28:38 -0300

    Actualizacion

--- config.js
+++ config.js
@@ -1,12 +1,9 @@
 const config = {
   appConfig: {
-    port: process.env.PORT || "5000",
-    host: process.env.HOST || "0.0.0.0",
+    port: process.env.PORT || 5000,
+    host: process.env.APP_HOST || "0.0.0.0",
   },
   dbConfig: {
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST,
-    dbName: process.env.DB_NAME,
     url: process.env.DB_URL,
   },
 };
